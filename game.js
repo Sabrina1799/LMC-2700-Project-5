7b89d4230750ee4e2d7327d8ca8e2f8f45424c78
KatShambaugh <44981851+KatShambaugh@users.noreply.github.com>
2018-11-25 21:07:32 -0500

    Update game.js

--- game.js
+++ game.js
@@ -9,7 +9,7 @@ class Game extends React.Component {
           Pick flowers and drag them to your plot to make music.
         </div>
         <div className="plants">
-        	<img src='https://github.com/Sabrina1799/LMC-2700-Project-5/blob/Kat/Flowers/5SidedYellow.png?raw=true'/>
+          <img src='https://github.com/Sabrina1799/LMC-2700-Project-5/blob/Kat/Flowers/5SidedYellow.png?raw=true'/>
           <img src='https://github.com/Sabrina1799/LMC-2700-Project-5/blob/Kat/Flowers/6SidedOrange.png?raw=true'/>
           <img src='https://github.com/Sabrina1799/LMC-2700-Project-5/blob/Kat/Flowers/6SidedPurple.png?raw=true'/>
           <img src='https://github.com/Sabrina1799/LMC-2700-Project-5/blob/Kat/Flowers/FancyPink.png?raw=true'/>
@@ -25,22 +25,22 @@ class Game extends React.Component {
           <img src='https://github.com/Sabrina1799/LMC-2700-Project-5/blob/Kat/Flowers/YellowDaisy.png?raw=true'/>
         </div>
         <div className="plots">
+        	<h6>Gardener One</h6>
         	<table className="plot" id="plot1">
-          <caption>First Player</caption>
         	<tr>
-            <td>Image 1</td><td>Image 2</td><td>Image 3</td><td>Image 4</td><td>Image 5</td><td>Image 6</td><td>Image 7</td>
+            <td><img src='https://github.com/Sabrina1799/LMC-2700-Project-5/blob/Kat/Plot.png?raw=true'/></td><td><img src='https://github.com/Sabrina1799/LMC-2700-Project-5/blob/Kat/Plot.png?raw=true'/></td><td><img src='https://github.com/Sabrina1799/LMC-2700-Project-5/blob/Kat/Plot.png?raw=true'/></td><td><img src='https://github.com/Sabrina1799/LMC-2700-Project-5/blob/Kat/Plot.png?raw=true'/></td><td><img src='https://github.com/Sabrina1799/LMC-2700-Project-5/blob/Kat/Plot.png?raw=true'/></td><td><img src='https://github.com/Sabrina1799/LMC-2700-Project-5/blob/Kat/Plot.png?raw=true'/></td><td><img src='https://github.com/Sabrina1799/LMC-2700-Project-5/blob/Kat/Plot.png?raw=true'/></td>
         	</tr>
         	<tr>
-            <td>Image 1</td><td>Image 2</td><td>Image 3</td><td>Image 4</td><td>Image 5</td><td>Image 6</td><td>Image 7</td>
+            <td><img src='https://github.com/Sabrina1799/LMC-2700-Project-5/blob/Kat/Plot.png?raw=true'/></td><td><img src='https://github.com/Sabrina1799/LMC-2700-Project-5/blob/Kat/Plot.png?raw=true'/></td><td><img src='https://github.com/Sabrina1799/LMC-2700-Project-5/blob/Kat/Plot.png?raw=true'/></td><td><img src='https://github.com/Sabrina1799/LMC-2700-Project-5/blob/Kat/Plot.png?raw=true'/></td><td><img src='https://github.com/Sabrina1799/LMC-2700-Project-5/blob/Kat/Plot.png?raw=true'/></td><td><img src='https://github.com/Sabrina1799/LMC-2700-Project-5/blob/Kat/Plot.png?raw=true'/></td><td><img src='https://github.com/Sabrina1799/LMC-2700-Project-5/blob/Kat/Plot.png?raw=true'/></td>
         	</tr>
     			</table>
+          <h6>Gardener Two</h6>
           <table className="plot" id="plot2">
-          <caption>Second Player</caption>
         	<tr>
-            <td>Image 1</td><td>Image 2</td><td>Image 3</td><td>Image 4</td><td>Image 5</td><td>Image 6</td><td>Image 7</td>
+            <td><img src='https://github.com/Sabrina1799/LMC-2700-Project-5/blob/Kat/Plot.png?raw=true'/></td><td><img src='https://github.com/Sabrina1799/LMC-2700-Project-5/blob/Kat/Plot.png?raw=true'/></td><td><img src='https://github.com/Sabrina1799/LMC-2700-Project-5/blob/Kat/Plot.png?raw=true'/></td><td><img src='https://github.com/Sabrina1799/LMC-2700-Project-5/blob/Kat/Plot.png?raw=true'/></td><td><img src='https://github.com/Sabrina1799/LMC-2700-Project-5/blob/Kat/Plot.png?raw=true'/></td><td><img src='https://github.com/Sabrina1799/LMC-2700-Project-5/blob/Kat/Plot.png?raw=true'/></td><td><img src='https://github.com/Sabrina1799/LMC-2700-Project-5/blob/Kat/Plot.png?raw=true'/></td>
         	</tr>
         	<tr>
-            <td>Image 1</td><td>Image 2</td><td>Image 3</td><td>Image 4</td><td>Image 5</td><td>Image 6</td><td>Image 7</td>
+            <td><img src='https://github.com/Sabrina1799/LMC-2700-Project-5/blob/Kat/Plot.png?raw=true'/></td><td><img src='https://github.com/Sabrina1799/LMC-2700-Project-5/blob/Kat/Plot.png?raw=true'/></td><td><img src='https://github.com/Sabrina1799/LMC-2700-Project-5/blob/Kat/Plot.png?raw=true'/></td><td><img src='https://github.com/Sabrina1799/LMC-2700-Project-5/blob/Kat/Plot.png?raw=true'/></td><td><img src='https://github.com/Sabrina1799/LMC-2700-Project-5/blob/Kat/Plot.png?raw=true'/></td><td><img src='https://github.com/Sabrina1799/LMC-2700-Project-5/blob/Kat/Plot.png?raw=true'/></td><td><img src='https://github.com/Sabrina1799/LMC-2700-Project-5/blob/Kat/Plot.png?raw=true'/></td>
         	</tr>
     			</table>  
         </div>
